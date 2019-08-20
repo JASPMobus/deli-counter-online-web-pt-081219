08a9e7662f67fdc7c90d0b44659ebe29d1d98c20
James Mobus <mobusj@live.com>
2019-08-20 23:44:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     
-    puts "#{katz_deli.each_with_index.inject{ |ret = "The line is currently:", index, patron| "#{ret} #{(index + 1).to_s}. #{patron}"}}"
+    puts "The line is currently: #{katz_deli.each_with_index.inject{ |ret = , index, patron| "#{ret} #{(index + 1).to_s}. #{patron}"}}"
   end
 end
