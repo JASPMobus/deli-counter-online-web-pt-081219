3c37a35c3eafef02c5c6cce65a4d60a356f5602d
James Mobus <mobusj@live.com>
2019-08-20 23:56:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     resp = "The line is currently:"
-    
+    counter = 0
     katz_deli.each_with_index do |patron, index|
       resp = resp + " #{(index + 1).to_s}. #{patron}"
     end
