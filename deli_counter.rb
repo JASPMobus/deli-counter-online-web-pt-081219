a31bcd3c9c2e7b4e505e3ce56bd397fc1d7f86f7
James Mobus <mobusj@live.com>
2019-08-21 00:04:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if(katz_deli.length==0) then
     puts "The line is currently empty."
   else
-    puts "#{katz_deli.each_with_index.inject { |ret = "The line is currently:", (patron, index = 1)| ret = ret + " #{(index+1).to_s}. #{patron}"}}"
+    puts "#{katz_deli.each_with_index.inject { |ret = "The line is currently:", (patron, index = 1)| ret = ret + " #{index.to_s}. #{patron}"}}"
     end
   end
 end
